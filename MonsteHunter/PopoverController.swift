809d44a2f0c208f88700a22956edcb697bd94930
Michael Arbogast <marboga@gmail.com>
2016-05-18 22:30:04 -0700

    Game functionality implemented.

--- MonsteHunter/PopoverController.swift
+++ MonsteHunter/PopoverController.swift
@@ -12,22 +12,79 @@ import MapKit
 class PopoverController: UIViewController {
     
     @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var healthLabel: UILabel!
     
-    let imageArray: [String] = ["pika", ]
+    var nameArray = ["Pikachuu", "Brain in a Jar", "Pirandha Bag", "Gelatinous Cube"]
+    
+    var dicto: [[AnyObject]] = [["Pikachuu", 100, "Electric"],["Brain in a Jar", 250, "Psychic"],["Pirandha Bag", 70, "Leather"],["Gelatinous Cube", 500, "Jelly"]]
+    
+    let imageArray = ["pika", "jarby", "piradha", "gelatinous_cube"]
+    
+    let randNum = Int(arc4random_uniform(3))
+    
+    var allowCatches = false
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
     
     var annotation = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("HAIIII")
-        imageOutlet.image = UIImage(named: "pika")
+        
+        allowCatches = false
+        
+        imageOutlet.image = UIImage(named: "\(imageArray[randNum])")
+        nameLabel.text = "\(dicto[randNum][0])"
+        healthLabel.text = "Health: \(dicto[randNum][1])"
+        typeLabel.text = "Type: \(dicto[randNum][2])"
+        
+        switch typeLabel.text! {
+            case "Electric":
+                typeLabel.backgroundColor = UIColor.yellowColor()
+                typeLabel.textColor = UIColor.blackColor()
+            case "Leather":
+                typeLabel.backgroundColor = UIColor.brownColor()
+                typeLabel.textColor = UIColor.yellowColor()
+            case "Jelly":
+                typeLabel.backgroundColor = UIColor.greenColor()
+                typeLabel.textColor = UIColor.whiteColor()
+            default:
+                typeLabel.textColor = UIColor.brownColor()
+        }
     }
     
     @IBAction func runAwayAction(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func fightButtonPressed(sender: UIButton) {
+        let attackNum: Int = Int(arc4random_uniform(35) + 4)
+        
+        let life = dicto[randNum][1]
+        print(life)
+        
+        let remainingLife: Int = Int(life as! NSNumber) - Int(attackNum)
+        if remainingLife > 0 {
+            healthLabel.text = "Health: \(remainingLife)"
+            let subtraction: Int = Int(life as! NSNumber) - remainingLife
+            dicto[randNum][1] = "\(subtraction)"
+                if subtraction < 50 {
+                healthLabel.textColor = UIColor.redColor()
+                allowCatches = true
+            }
+        } else {
+            print("You killed it.")
+            dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
     
+    @IBAction func ballButtonPressed(sender: UIButton) {
+        if allowCatches == true {
+            nameLabel.text = "Caught a \(dicto[randNum][0])!"
+        }
+    }
     
     
 }
